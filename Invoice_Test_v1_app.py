cd056baf30fa1a963b0309bd13d932a045129ae9
DemosthenisD <67370261+DemosthenisD@users.noreply.github.com>
2023-02-03 12:51:49 +0200

    Update Invoice_Test_v1_app.py

--- Invoice_Test_v1_app.py
+++ Invoice_Test_v1_app.py
@@ -1,6 +1,6 @@
 #This code example uses the streamlit and pandas libraries to create a table of invoices issued and allow authorized users to create new invoices.
-#The user can select a client from a drop-down list and then fill out a form with the relevant information to create a new invoice. 
-#The new invoice is then appended to the table of invoices issued and a success message is displayed. 
+#The user can select a client from a drop-down list and then fill out a form with the relevant information to create a new invoice.
+#The new invoice is then appended to the table of invoices issued and a success message is displayed.
 #The code also includes a placeholder for generating a word invoice based on a pre-defined template.
 
 import streamlit as st
@@ -11,9 +11,26 @@ data = {"Invoice No": [0], "Client": ["TEST"], "Date": ["1/1/2023"], "Project": 
 df = pd.DataFrame(data)
 
 # List of authorized clients
-clients = ["Client A", "Client B", "Client C"]
+clients = ["Client A", "Client B", "Client C", "Client D"]
 
-def new_invoice_form(client):
+# def new_invoice_form(client):
+#     invoice_no = 1 #len(df) + 1
+#     date = st.date_input("Date")
+#     project = st.text_input("Project")
+#     amount = st.number_input("Amount")
+#     vat = st.number_input("VAT")
+#     description = st.text_area("Description")
+#     create = st.button("Create")
+#     if create:
+#         df_new = pd.DataFrame({"Invoice No": [invoice_no], "Client": [client], "Date": [date], "Project": [project], "Amount": [amount], "VAT": [vat], "Description": [description]})
+#         df = df.append(df_new)
+#         st.success("Invoice created successfully!")
+#         # print the invoice in word based on the template
+#         # Code to generate the invoice based on the template goes here
+
+
+client = st.selectbox("Select Client", clients)
+if client:
     invoice_no = 1 #len(df) + 1
     date = st.date_input("Date")
     project = st.text_input("Project")
@@ -28,10 +45,6 @@ def new_invoice_form(client):
         # print the invoice in word based on the template
         # Code to generate the invoice based on the template goes here
 
-
-client = st.selectbox("Select Client", clients)
-if client:
-  new_invoice_form(client)
 st.write("Invoices Issued:", df)
 
 #def main():
@@ -42,7 +55,3 @@ st.write("Invoices Issued:", df)
 
 #if __name__ == "__main__":
 #    main()
-
-
-
-
